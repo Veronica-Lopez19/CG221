3d163d0fe0ef03343228014572e73d38cd6d652b
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-04-05 21:07:11 -0400

    Add files via upload

---
+++ jquery/arrowscript.js
@@ -0,0 +1,6 @@
+//$(".arrow").hover(function(){
+  //$(".arrow").animate({left: '100%'});
+//alert("clicked arrow");
+    $(".main-container").fadeOut(2000);
+
+//});
--- jquery/script.js
+++ jquery/script.js
@@ -1,3 +1,7 @@
-$(document).ready(function{
-  alert("Good");
+$(document).ready(function(){
+  // alert("Good");
+  $('.main-container').click(function(){
+    $('.arrow').animate({left: '420%'});
+  });
+
 });
