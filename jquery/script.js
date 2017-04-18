75f1b7404cb5eb5266cdbc0a2ea7a61917fad760
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-04-18 12:52:23 -0400

    Update script.js

--- jquery/script.js
+++ jquery/script.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
   $('.main-container').click(function(){
     $('.arrow').animate({left: '420%'});
   });
-  $(document).keypress(function (e) {
-  if (e.keyCode === '32') {
-    e.preventDefault()
-    console.log('Space pressed')
-  }
-});
+ // $(document).keypress(function (e) {
+ // if (e.keyCode == 0 ||e.keyCode === 32) {
+   // e.preventDefault()
+   // console.log('Space pressed')
+  //}
+//});
+   $(document).keydown(function(e) {
+    if (e.keyCode == '32') {
+        alert('space');
+        }
+       });
 });
