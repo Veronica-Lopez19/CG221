96d2f575704745354849e461014a2d196b00b47c
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-05-02 13:34:02 -0400

    Update sketch.js

--- p5start/sketch.js
+++ p5start/sketch.js
@@ -2,18 +2,34 @@ var party = 60;
 var r = 30;
 var g = 100;
 var b = 200;
+var shape;
 function setup() {
 
   //changed background color to black
-
+createCanvas(windowWidth, windowHeight);
+shape = new Jitter();
 }
 function draw() {
-  //make my canvas
-  createCanvas(windowWidth, windowHeight);
-
+  shape.move();
+  shape.display();
   fill(r, mouseY, b);
   ellipse(mouseX, mouseY, party, party);
-  push()
+  rec(60,60,60,60)
+  ellipse.displace(rec)
   
+} 
+function Jitter() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(10, 30);
+  this.speed = 1;
 
-}
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  };
+
+  this.display = function() {
+    rec(this.x, this.y, 10, 10);
+  }
+};
