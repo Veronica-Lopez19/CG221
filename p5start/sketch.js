9844f828942cf5524f38f856e3bc30073405368d
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-05-03 19:35:56 -0400

    Update sketch.js

--- p5start/sketch.js
+++ p5start/sketch.js
@@ -1,26 +1,22 @@
 var cloud, circle;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);  
- 
+  createCanvas(windowWidth, windowHeight);
   cloud = createSprite(600, 200);
-
   circle = createSprite(600, 200);
- 
-  
-  
 }
 
 function draw() {
-  background('#8e9aad');  
-  
+  background('#8e9aad');
+
   circle.position.x = mouseX;
   circle.position.y = mouseY;
 
   circle.displace(cloud);
-    
+
+
   circle.debug = mouseIsPressed;
   cloud.debug = mouseIsPressed;
-  
+
   drawSprites();
 }
