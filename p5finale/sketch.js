329db17bf4b5d0ced5d2d77aaa9b1edf15ce7246
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-05-10 20:57:46 -0400

    Add files via upload

--- p5finale/sketch.js
+++ p5finale/sketch.js
@@ -1,10 +1,38 @@
-var
+var img;
+var img2;
+var x = 180;
+var y = 180;
+var xspeed = 9;
+var yspeed = 10
+function preload() {
+  img = loadImage("ninja.png");
+  img2 = loadImage("shuriken.png");
+
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  createCanvas(1000, 800);
+
 }
 
 function draw() {
-  background('#8e9aad');
+  background('#a4b8d8');
+
+  image(img,mouseX-50, mouseY-80, img.width/20, img.height/20);
+
+  image(img2, x, y, img.width/40, img.height/45);
+
+    x = x + xspeed;
+
+  	 if (x > windowWidth || x < 0)  {
+     	xspeed = -xspeed;
+  	}
+
+  	//bouncing veritcally
+  	y = y + yspeed;
+
+  	if (y > windowHeight || y < 0) {
+	 	  yspeed = -yspeed;
+    }
 
 }
