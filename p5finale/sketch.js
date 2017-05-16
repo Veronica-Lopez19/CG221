769fbc7a02c3803a12c7cf38cca7907c65e42096
Veronica-Lopez19 <veronica_lopez@fitnyc.edu>
2017-05-16 10:27:55 -0400

    Update sketch.js

--- p5finale/sketch.js
+++ p5finale/sketch.js
@@ -9,7 +9,7 @@ var yspeed = 18;
 function preload() {
   img = loadImage("ninja.png");
   img2 = loadImage("shuriken.png");
-
+  back2= loadImage("back2.png")
 }
 
 function setup() {
@@ -18,8 +18,9 @@ function setup() {
 }
 
 function draw() {
-  background('#a4b8d8');
-
+  //background('#a4b8d8');
+  background('back2.png')
+	
   image(img,mouseX-50, mouseY-80, img.width/10, img.height/10);
 
   image(img2, x, y, img.width/20, img.height/25);
